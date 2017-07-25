47d49537ac295868b6c11e1df2501879fb3748b8
solaguilera <s-aguilera@live.com>
2017-07-25 11:53:51 -0300

    functions

--- app/src/containers/AmenitiesContainer.jsx
+++ app/src/containers/AmenitiesContainer.jsx
@@ -5,24 +5,30 @@ export default class Form extends React.Component {
 	constructor(props) {
 		super(props);
 		this.initiateBooking=this.initiateBooking.bind(this);
+		this.state = {
+		amenities:this.props.amenities,
+		date:this.props.date,
+		time:this.props.time,
+		comments:this.props.comments,
+		//isEditing: false
 	},
 	initiateBooking(e) {
 		preventDefault(e);
-		const Amenities = this.refs.Amenities.value;
-		const date = this.refs.Date.value;
-		const Time = this.refs.Time.value;
-		const Comments= this.refs.Comments.value;
-		this.props.createBooking(Amenities, date, Time, Comments);
+		const amenities = this.refs.amenities.value;
+		const date = this.refs.date.value;
+		const time = this.refs.time.value;
+		const comments= this.refs.comments.value;
 		this.refs.AmenitiesForm.reset();
+		return this.setState({amenities, date, time, comments});
 	},
 	render() {
 		return (
 			<div>
 				<Form ref='AmenitiesForm' className='AmenitiesForm'>
-			        <input ref='Amenities' type='text' placeholder='Amenities'></input>
-			        <input ref='Date' type='date' min="2017-07-01" placeholder='Fecha'></input>
-			        <input ref='Time' type='time' placeholder='Horario'></input>
-			        <textarea ref='Comments' rows="4" cols="50" placeholder='Comentarios'></textarea>
+			        <input ref='amenities' type='text' placeholder='Amenities'></input>
+			        <input ref='date' type='date' min="2017-07-01" placeholder='Fecha'></input>
+			        <input ref='time' type='time' placeholder='Horario'></input>
+			        <textarea ref='comments' rows="4" cols="50" placeholder='Comentarios'></textarea>
 			        <SubmitButton onClick={this.initiateBooking}/>
 		        </Form>
 			</div>
