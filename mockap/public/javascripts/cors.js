99ed2076751950925346bef67a8a564ef08eb662
socramreysa <you@example.com>
2017-08-14 18:58:34 -0300

    marcos

--- mockap/public/javascripts/cors.js
+++ mockap/public/javascripts/cors.js
@@ -5,7 +5,7 @@ const allowCrossOrigin = function(req, res, next) {
 			'Access-Control-Allow-Origin': 'http://localhost:8080',
 			'Access-Control-Allow-Credentials': true,
 			'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-			"Access-Control-Allow-Headers": "Origin, X-Requested-With, Accept, Header, Content-Type, access-control-allow-origin",
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Accept, Header, Content-Type, access-control-allow-origin',
 			'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
 		});
 		return res.sendStatus(200);
@@ -13,7 +13,7 @@ const allowCrossOrigin = function(req, res, next) {
 	res.set({
 		'Access-Control-Allow-Origin': 'http://localhost:8080',
 		'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-		'Access-Control-Allow-Headers':"header, Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since",
+		'Access-Control-Allow-Headers':'header, Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since',
 		'Access-Control-Allow-Credentials': true,
 	});
 	next();
